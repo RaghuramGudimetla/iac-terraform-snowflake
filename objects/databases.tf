80f8b7c9fe2a35c6ebd79e572e7fe1d3736e0a06
Raghuram Gudimetla <gudimetla.raghu36@gmail.com>
2023-06-28 13:30:33 +1200

    Adding a landing database

--- objects/databases.tf
+++ objects/databases.tf
@@ -1,4 +1,14 @@
-resource "snowflake_database" "raw_landing" {
-  name                        = "raw_landing"
+terraform {
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.67.0"
+    }
+  }
+}
+
+# Landing
+resource "snowflake_database" "landing" {
+  name                        = upper("landing")
   comment                     = "Database where all the raw data lands in raw format"
 }
